5661f06f3bf3da487cf6ce1d019cf4daa4022295
uaykul <uaykul@gmail.com>
2022-10-18 16:52:35 +0300

    check

--- sw.js
+++ sw.js
@@ -1,13 +1,27 @@
 self.addEventListener("push", (e) => {
-    
+
   const config = {
     body: "Update",
     data: {
       dateOfArrival: Date.now(),
       primaryKey: "3",
     },
+    icon:"assent/logoKsgY.png",
+    vibrate: [100, 50, 100],
+    actions: [
+      {
+        action: "explore",
+        title: "Action1",
+        // icon: "images/"
+      },
+      {
+        action: "close",
+        title: "Bildirimi Kapat",
+        // icon:
+      },
+    ],
   };
   e.waitUntil(
-    self.registration.showNotification(" New Version Avaible", config)
+    self.registration.showNotification("New Version Avaible", config)
   );
 });
